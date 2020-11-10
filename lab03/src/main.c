d05c55d305095dfb4ceff1391dc34ef1cc76cca6
conf1cker <m897036@gmail.com>
2020-11-09 20:33:47 -0500

    lab03real

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -6,43 +6,43 @@ int main()
 	#define R3 18
 	float R;
 	R = ( R1 * R2 * R3 ) / ( R2 * R3 + R1 * R3 + R1 * R2 );
-
+	return 0;
 
 	//task 2
-	const int A = 321;
-	int a = A / 100;
-	int b = A / 10 % 10;
-	int c = A % 10;
+	const int X = 321;
+	int a = X / 100;
+	int b = X / 10 % 10;
+	int c = X % 10;
 	int result = c * 100 + b * 10 + a;
-
+	return 0;
 
 	//task 3
-	const float A = 4;
+	const float X = 4;
 	a = a * a; //2
 	a = a * a; //4
-	a = A;
+	a = X;
 	a = a * a * a; //3
         a = a * a; //6
-        a = A;
+        a = X;
         a = a * a; //2
         a = a * a; //4
         a = a * a; //8
-	a = A;
+	a = X;
 	a = a * a; //2
 	float b = a * a; //4
 	b = b * b; //8
 	a = b * a; //10
-
+	return 0;
 
 	//task 4
 	const int P = 8;
-	int a = 4512;
-	int a1 = a / 1000; //4
-	int a2 = a / 100 % 10; //5
-	int a3 = a / 10 % 10; //1
-	int a4 = a % 10; //2
-	int result = a4 * 1 + a3 * P + a2 * P * P + a1 * p * p * p;
-
+	int x = 4512;
+	int x1 = a / 1000; //4
+	int x2 = a / 100 % 10; //5
+	int x3 = a / 10 % 10; //1
+	int x4 = a % 10; //2
+	int result = x4 * 1 + x3 * P + x2 * P * P + x1 * p * p * p;
+	return 0;
 
 	//task 5
 	const int X = 123;
@@ -52,13 +52,12 @@ int main()
 	y = y * 100; // 33.3333
 	int z = (int) y; //33
 	float result = (float) z / 100; //0.3300000
-
+	return 0;
 
 	//task 6
 	const float A = 50;
 	const float B = 52;
 	float sum;
 	sum = (( A + B ) * ( B - A + 1 ));
-
 	return 0;
 }
